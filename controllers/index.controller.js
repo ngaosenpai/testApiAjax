8cb25da28119f72a4a86fa10d8378dc76933ae5d
ngaosenpai <longbestlee1909@gmail.com>
2020-03-07 23:35:06 +0700

    done

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,20 +1,22 @@
 
 module.exports.index = (req, res) => {
 	res.render("index", {
-		include: "components/trangchu.pug",
+		page : 0,
 		pageName: "Trang chu"
 	})
 }
 
 module.exports.routing = (req, res) => {
 	const pages = {
-		"about" : "components/about.pug",
-		"service" : "components/service.pug",
-		"contact" : "components/contact.pug",
+		"about" : 1,
+		"service" : 2,
+		"contact" : 3,
 	}
 	if(req.params.page in pages)
 		res.render("index", {
-			include: pages[req.params.page],
+			page: pages[req.params.page],
 			pageName: `Page: ${req.params.page}`
 		})
+	else
+		res.sendStatus(404)
 }
